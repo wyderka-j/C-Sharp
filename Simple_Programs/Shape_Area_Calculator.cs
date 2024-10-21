7e7f868f69486cec043a6a82a3c51b14a260fb6e
wyderka-j <131911420+wyderka-j@users.noreply.github.com>
2024-10-21 18:41:42 +0200

    Add files via upload

---
+++ Simple_Programs/Shape_Area_Calculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ShapeArea
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            float result;
+
+            Console.WriteLine("What shape would you like to find the area of?");
+            Console.WriteLine("Please enter 'r' for rectangle or 'c' for circle or any other key for triangle.");
+
+            answer = Console.ReadLine();
+
+            if (answer == "r")
+            {
+                Console.WriteLine("Please enter the height of the rectangle.");
+                float height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the width of the rectangle.");
+                float width = float.Parse(Console.ReadLine());
+
+                result = height * width;
+            }
+            else if (answer == "c") 
+            {
+                Console.WriteLine("Please enter the radius of the circle.");
+                float radius = float.Parse(Console.ReadLine());
+
+                result = (float)Math.PI * (radius * radius);
+            }
+            else
+            {
+                Console.WriteLine("Please enter the height of the triangle.");
+                float height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the the base of the triangle.");
+                float width = float.Parse(Console.ReadLine());
+
+                result = (height * width) /2;
+            }
+
+            Console.WriteLine("The result is " + result);
+            Console.ReadKey();
+        }
+    }
+}
